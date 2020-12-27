802e5cfefbaf4a2176f0f5172a8352870f3fcfee
rtolps <rstolppi@yahoo.com>
2020-12-26 20:38:58 -0500

    Update convert_to_onnx.py

--- convert_to_onnx.py
+++ convert_to_onnx.py
@@ -1,18 +1,31 @@
 import torch.onnx
 from torch import nn
 
-import config
+from utils import *
 from dataset import ImageFolder
 from networks import *
 
-class Model(object, input_nc, output_nc, ngf=64, n_blocks=6, img_size=256, light=False):
+class Model(object) :
     def __init__(self):
         super().__init__()
-        self.n_res = args.n_res
-        
+        self.genA2B = ResnetGenerator(input_nc=3, output_nc=3, 
+                                      ngf=64, n_blocks=4, img_size=256, light=True).to('cuda')
+
     def forward(self, x):
-        out = self.n_res(x)
+        out = self.genA2B(x)
         out = nn.functional.interpolate(out, scale_factor=2, 
                                         mode='bilinear', align_corners=True)
         out = torch.nn.functional.softmax(out, dim=1)
         return out
+
+model = Model()
+model.eval()
+random_input = torch.randn(3, 3, 256, 256, dtype=torch.float32)
+# you can add however many inputs your model or task requires
+ 
+input_names = ["real_A"]
+output_names = ["fake_A2B"]
+ 
+torch.onnx.export(model, random_input, 'model.onnx', verbose=False, 
+                  input_names=input_names, output_names=output_names, 
+                  opset_version=11)
